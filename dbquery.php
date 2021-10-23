81ae9daa66e66d56068c5f60f8068cd3743ac619
Aydin <aydin@lyka.com.au>
2021-10-24 00:31:37 +1100

    Revert "Issue #2 Improved Questions page UI"
    This reverts commit cf6bd8db9cb55b91b301b69ff989f4aba09cd8de.

--- dbquery.php
+++ dbquery.php
@@ -1,56 +1,56 @@
 <?php
-    function get_data_from_db($con, $section, $query, $parameterString)
-    {
-        if ($con === false) {
-            die("ERROR: Could not connect. " . mysqli_connect_error());
-        }
+function get_data_from_db($con,$section,$query,$parameterString){
+if($con === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+    echo 'Cannot connect to database';
+}
+else{
+    $stmt= mysqli_stmt_init($con);
+    if(!mysqli_stmt_prepare($stmt, $query))
+{
+    print "Failed to prepare statement\n";
+}
+mysqli_stmt_bind_param($stmt, $parameterString, $section);
+    
+mysqli_stmt_execute($stmt);
+    // $sql="SELECT * FROM `uploadimage` WHERE Section = 'cultural' ORDER BY timestamp DESC";
+     // $imageInfos=mysqli_query($con,$sql);
+    $result = mysqli_stmt_get_result($stmt);
+    mysqli_stmt_close($stmt);
+   return $result;
 
-        $stmt= mysqli_stmt_init($con);
-        if(!mysqli_stmt_prepare($stmt, $query)) {
-            print "Failed to prepare statement\n";
-        }
+}
 
-        mysqli_stmt_bind_param($stmt, $parameterString, $section);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        mysqli_stmt_close($stmt);
+}
 
-        return $result;
-    }
-
-    function Validate_Ip($IP)
-    {
-        if (filter_var($IP, FILTER_VALIDATE_IP)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    function checkVpn($ip){
-        $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-        if ($details->country != "IN"){
-            $mac=exec('getmac');
-            $mac=strtok($mac,' ');
-            echo '<script>alert("Oh using VPN now are we.\nDont worry I got more ways to track u than u can imagine\nSo kindly just stick to using this site for good.");document.location="index.php";</script>';
-            return false;
-        }
+function Validate_Ip($IP)
+{    
+    if (filter_var($IP, FILTER_VALIDATE_IP)) {
         return true;
+    } else {
+        return false;
     }
+}
+function checkVpn($ip){
+    $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+    if ($details->country != "IN"){
+        $mac=exec('getmac');
+        $mac=strtok($mac,' ');
+        echo '<script>alert("Oh using VPN now are we.\nDont worry I got more ways to track u than u can imagine\nSo kindly just stick to using this site for good.");document.location="index.php";</script>';
+        return false;
+    }
+    return true;
+}
 
-    function add_ip($ip,$con) {
-        $query= "INSERT INTO ipaddr (`ip`)VALUES(?)";
-        $stmt= mysqli_stmt_init($con);
-        if(!mysqli_stmt_prepare($stmt, $query)) {
-            print "Failed to prepare statement\n";
-        }
-        mysqli_stmt_bind_param($stmt, "s", $ip);
-        mysqli_stmt_execute($stmt);
-    }
+function add_ip($ip,$con){
+    
+    $query= "INSERT INTO ipaddr (`ip`)VALUES(?)";
+$stmt= mysqli_stmt_init($con);
+if(!mysqli_stmt_prepare($stmt, $query))
+{
+print "Failed to prepare statement\n";
+}
+mysqli_stmt_bind_param($stmt, "s", $ip);
+mysqli_stmt_execute($stmt);
 
-    function getQuestions($con, $subject)
-    {
-        $q='SELECT * from ques where subject = ? ORDER BY time';
-        $p="s";
-        $result = get_data_from_db($con, $subject, $q, $p);
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
+}
