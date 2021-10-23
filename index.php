b5bf54c79f69cfa1ba957288091a46a117be1476
Aydin <aydin@lyka.com.au>
2021-10-23 23:42:40 +1100

    Issue #1 Improved index.php UI

--- index.php
+++ index.php
@@ -1,36 +1,76 @@
 <!DOCTYPE html>
 <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1"
-      />
+    name="viewport"
+    content="width=device-width, initial-scale=1, maximum-scale=1"
+/>
 <html>
     <head>
-    <title>
-Viva Ques
-    </title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<style>
-    a{
-        display: block;
-        margin:20px 20px;
-        font-size: 1.3em;
-    }
-</style>
+        <title>
+            Viva Ques
+        </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css" />
     </head>
- 
-<body>
-    <div style="width:80%; margin:auto; margin-top:7em">
-
-        <div class="row" style="justify-content:space-around;">
-<a href="ques.php?sub=css">CSS</a>
-<a href="ques.php?sub=dwm">DWM</a>
-<a href="ques.php?sub=se">SE</a>
-<a href="ques.php?sub=spcc">SPCC</a>
-</div>
-<div class="row" style="justify-content:space-around;">
-<a href="add.php">Add Ques</a>
-</div></div>
-<div class="row" style="background-color: black;"><marquee style="color:white; font-size:20px">ASSDF ROCKS!</div>
-</div>
-</body>
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>ASSDF ROCKS!</h1>
+            </div>
+            <div class="row justify-content-between">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">CSS</h5>
+                        <?php for ($i=2; $i<=8; $i++): ?>
+                            <p class="card-text">
+                                <a href="ques.php?sub=css&sem=<?=$i?>">
+                                    Semester <?=$i?>
+                                </a>
+                            </p>
+                        <?php endfor; ?>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">DWM</h5>
+                        <?php for ($i=2; $i<=8; $i++): ?>
+                            <p class="card-text">
+                                <a href="ques.php?sub=dwm&sem=<?=$i?>">
+                                    Semester <?=$i?>
+                                </a>
+                            </p>
+                        <?php endfor; ?>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">SE</h5>
+                        <?php for ($i=2; $i<=8; $i++): ?>
+                            <p class="card-text">
+                                <a href="ques.php?sub=se&sem=<?=$i?>">
+                                    Semester <?=$i?>
+                                </a>
+                            </p>
+                        <?php endfor; ?>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">SPCC</h5>
+                        <?php for ($i=2; $i<=8; $i++): ?>
+                            <p class="card-text">
+                                <a href="ques.php?sub=spcc&sem=<?=$i?>">
+                                    Semester <?=$i?>
+                                </a>
+                            </p>
+                        <?php endfor; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-2">
+                <a type="button" class="btn btn-primary" href="add.php">
+                    Add Ques
+                </a>
+            </div>
+        </div>
+    </body>
 </html>
